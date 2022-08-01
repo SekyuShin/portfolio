8fe515e0308756f992466efbb3104388bc795ef2
SK <doublesk0810@gmail.com>
2022-08-02 03:48:11 +0900

    서버, React 연동 + DB 전달을 위하여 React axios test 진행중

--- app.js
+++ app.js
@@ -3,15 +3,30 @@
 // node_modules의 express 패키지를 가져오고 app에서 express()함수를 실행한다. 
 var express = require('express');
 var app = express();
+var path = require('path');
+var cors = require('cors');
+
+app.use(express.json());
+app.use(cors());
+
+//DB 전달 예시, client-side rendering
+app.get('/test',function(req,res){
+    res.json({name:'black shoes'});
+})
+
+//해당 경로의 static file들을 사용하겠다.
+app.use(express.static(path.join(__dirname,'client/build'))); 
 
 //get or post으로 접근 가능하다.
 //url로 접근하면 get
 // '/' 홈에 접근을 의미
 app.get('/', function(req, res) {
-    res.send("<h1>Express server Start</h1>");
+    console.log('Connect client');
+    res.sendFile(path.join(__dirname,'client/build/index.html'));
 })
 
+
 // express 서버를 실행할 때 필요한 포트 정의 및 실행 시 callback 함수를 받습니다
-app.listen(3000, function() {
+app.listen(3030, function() {
     console.log('start! express server');
 })
--- client/src/components/sections/Projects.js
+++ client/src/components/sections/Projects.js
@@ -4,6 +4,7 @@ import Paper from '@mui/material/Paper';
 import Card from '@mui/material/Card';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
+import Axios from 'axios';
 
 const Item = styled(Paper)(({ theme }) => ({
     backgroundColor: '#1A2027',
@@ -15,12 +16,19 @@ const Item = styled(Paper)(({ theme }) => ({
   }));
 
 
-
-
 const Projects = () => {
     //역시 관련프로젝트들 DB로 부터 가져와 출력예정
     //간략한 정보
     //자세한 정보는 클릭 이벤트를 통해 다이얼로그 생성 
+    const test = Axios.get('http://localhost:3030/test')
+    .then(function(response) {
+        return response.json();
+    })
+    .catch(function(error) {
+        console.log("error");
+        return ({name:'tt'});
+    })
+
     return (
       <>
         <Typography align='center' variant='h2' color='secondary.light'>PROJECTS</Typography>
@@ -39,6 +47,7 @@ const Projects = () => {
                 <Grid item xs={6}>
                     <Item>
                         <div>포트폴리오 사이트 개발</div>
+                        <div>{test.name}</div>
                         <div>자신을 어필하기 위한 포트폴리오 사이트 개발</div>
                         <div>Skill : React, Node.js, HTML, JavaScript, CSS</div>
                     </Item>
