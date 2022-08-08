84d2990aa8aafc44c07153b40ac01b6b0be5aa73
SK <doublesk0810@gmail.com>
2022-08-08 23:45:33 +0900

    client에서 server에 접근할 API 생성, Node.js에서 MySQL에 접근 API에 따라 sql문 생성 후 요청, 현재 연속된 요청 시 오류 발생

--- connMySql.js
+++ connMySql.js
@@ -9,18 +9,29 @@ const connection = mysql.createConnection({
 })
 
 /* parsing path and return sql */
+/*
+    projects - main project - all (간략 내용) req api : http://localhost:3030/projects/main/all
+                                              req DB "SELECT id, type, name, descriptions FROM testDB.projects WHERE type = \"Main Projects\";";
+                            - id (자세한 내용) req api : http://localhost:3030/projects/main/2
+                                              req DB "SELECT * FROM testDB.projects WHERE type=\"Main Projects\" AND id = 2;";
+             - side project - all (간략 내용)
+                            - id (자세한 내용)
+             - school project - all (간략 내용)
+                              - id (자세한 내용)
+*/
 function pathParsing (path){
-    var sql = "SELECT * FROM testDB.projects;"; 
+    var sql = "SELECT id, type, name, descriptions FROM testDB.projects WHERE type = \"Main Projects\";";
+    //var sql = "SELECT * FROM testDB.projects WHERE type=\"Main Projects\" AND id = 2;";
+    var projectType = "";
+    var id = "";
     const tmpPath = path.split('/');
     if(tmpPath.length<4) return '';
-    
-    //sql = sql + tmpPath[2]+tmpPath[3];
-        
-    if(tmpPath[3] === 'All') {
-        
-    } else {
+    if(tmpPath[2] === 'main') projectType = "Main Projects";
+    else if(tmpPath[2] === 'side') projectType = "Side Projects";
+    else if(tmpPath[2] === 'school') projectType = "School Projects";
 
-    }
+    if(tmpPath[3] === 'all') sql = "SELECT id, type, name, descriptions FROM testDB.projects WHERE type = \"" + projectType + "\";";
+    else sql = "SELECT * FROM testDB.projects WHERE type = \"" + projectType + "\" AND id = "+tmpPath[3] + ";";
     
     return sql;
 }
@@ -28,10 +39,12 @@ function pathParsing (path){
 exports.connect = function(path) {
     connection.connect();
     const sql = pathParsing(path);
+    
     console.log("sql :`${sql}` : ",sql);
     connection.query(sql, function(err, rows, fields){
         if(err) console.log('err : '+err);
         console.log(rows);
         });
-    connection.end();
+    //connection.end(); //Error code: 'PROTOCOL_ENQUEUE_AFTER_QUIT',
+                        //code: 'PROTOCOL_ENQUEUE_HANDSHAKE_TWICE',
 }
